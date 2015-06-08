d70356c838ccf3121c6513214ea584e58f19df87
eduardopoleo <eduardopoleo@gmail.com>
2015-06-08 15:56:52 -0400

    Working partially

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -10,3 +10,83 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+
+var FilterableProductTable = React.createClass({
+  render: function() {
+    return (
+      <div className='filterable-table'>
+        <SearchBar />
+        <ProductTable />
+      </div>
+    );
+  }
+});
+
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+      <form>
+        <input type='text' placeholder='Search product'> </input>
+        <br />
+        <input type='checkbox'>Only show products in stock</input>
+      </form>
+    );
+  }
+});
+
+var ProductTable = React.createClass({
+  render: function() {
+    // var rows = [];
+    // var lastCategory = null;
+    // {this.props.products}.forEach(function(product){
+    //  if (product.category != lastCategory)
+    //   rows.push()
+
+    // });
+
+    return (
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Price</th>
+        </thead>
+        <tbody>
+          <CategoryRow />
+          <ProductRow />
+        </tbody>
+      </table>
+    );
+  }
+});
+
+var CategoryRow = React.createClass({
+  render: function() {
+    return (
+      <div>
+       category row
+      </div>
+    );
+  }
+});
+
+var ProductRow = React.createClass({
+  render: function() {
+    return (
+      <div>
+       product row
+     </div>
+    );
+  }
+});
+
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+React.render(<FilterableProductTable />, document.getElementById('content') )
