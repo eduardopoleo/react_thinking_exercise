5a9fa9b53789781f5738fd1cc9f65247530b7c3f
Eduardo Poleo <eduardopoleo@gmail.com>
2015-06-08 23:47:08 -0400

    tutorial done

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -9,20 +9,29 @@
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
+//Note about data flow.
+// Only by setting the this.state we are just declaring that that specific property is a state
+// but we are still not making it dynamic. We have to:
+  // define the events that will make that state to change 
+  // use set.state to effectively change setState() of the component
+  // when there are changes is when we have to define the events handlers!! and the refs!!
 var FilterableProductTable = React.createClass({
   // return can be used either way depending on the actual 
   // value that is returned. In this case I am returning a 
   // hash so it makes sense to use curly braces
-
   // The minimal info required to determined the state of our system
-  // are the search term and the checkbox value. Everything else can be updated from this
+  // are the search term and the check-box value. Everything else can be updated from this
   // The state is better defined on the top level component and passed in as props to 
   // the inner resources. This is what is known as one-way data flow
-
-  // IMP: states do not come from anywhere like props! 
+    // IMP: states do not come from anywhere like props! 
   // But in turn we have to set an initial state so that the thing do not crash on the first run  
-  
+  handleUserInput: function(filterText, inStockOnly){
+    this.setState({
+      filterText: filterText,
+      inStockOnly: inStockOnly
+    });
+  },
+
   getInitialState: function(){
    return{
      filterText: '',
@@ -36,6 +45,7 @@ var FilterableProductTable = React.createClass({
         <SearchBar
          filterText = {this.state.filterText}
          inStockOnly={this.state.inStockOnly}
+         onUserInput={this.handleUserInput} 
         />
         <ProductTable
          products = {this.props.products}
@@ -48,14 +58,35 @@ var FilterableProductTable = React.createClass({
 });
 
 var SearchBar = React.createClass({
+  handleChange: function(){
+    this.props.onUserInput(
+      this.refs.filterTextInput.getDOMNode().value,
+      this.refs.inStockOnlyInput.getDOMNode().checked
+    );
+  },
+
   render: function() {
     return (
       <form>
-        <input type='text' placeholder='Search product' value = {this.props.filterText}> </input>
+        <input
+          type='text'
+          placeholder='Search product'
+          value={this.props.filterText}
+          ref='filterTextInput'
+          onChange={this.handleChange}
+        />
+
         <br />
-        <input type='submit'> </input>
-        <br />
-        <input type='checkbox' value={this.props.inStockOnly}>Only show products in stock</input>
+        <p>
+          <input
+            type='checkbox'
+            value={this.props.inStockOnly}
+            ref='inStockOnlyInput'
+            onChange={this.handleChange}
+          />
+          {' '}
+           Only show products in stock
+         </p>
       </form>
     );
   }
@@ -80,17 +111,22 @@ var ProductTable = React.createClass({
       last_category = product.category;
       //What does bind do? Why does the app breaks without it?
     }.bind(this));
+    console.log(rows)
 
     //how come this stuff gets parse out properly in here if it is passed as an array
     //We can not comment inside react components, it actually gets display in the 
     //browser
+    //IMP: How does this works <tbody>{rows}</body>? is an array that is passed in? 
+    // This a bit of black magic
     return (
       <table>
         <thead>
-          <th>Name</th>
-          <th>Price</th>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
         </thead>
-        <tbody> {rows} </tbody>
+        <tbody>{rows}</tbody>
       </table>
     );
   }
@@ -100,7 +136,7 @@ var CategoryRow = React.createClass({
   render: function() {
     return (
       <tr>
-        <td><strong>{this.props.category}</strong></td>
+        <th><strong>{this.props.category}</strong></th>
       </tr>
     );
   }
@@ -108,7 +144,7 @@ var CategoryRow = React.createClass({
 
 var ProductRow = React.createClass({
   render: function() {
-    if (this.props.stocked === true){
+    if (this.props.stocked){
       var nameStyle = {
         color: 'black'
       }
