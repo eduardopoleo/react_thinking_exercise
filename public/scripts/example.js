9ebdcb1335b3a6d015676ba61adf03e009826683
Eduardo Poleo <eduardopoleo@gmail.com>
2015-06-08 22:13:56 -0400

    One way data flow

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -11,11 +11,37 @@
  */
 
 var FilterableProductTable = React.createClass({
+  // return can be used either way depending on the actual 
+  // value that is returned. In this case I am returning a 
+  // hash so it makes sense to use curly braces
+
+  // The minimal info required to determined the state of our system
+  // are the search term and the checkbox value. Everything else can be updated from this
+  // The state is better defined on the top level component and passed in as props to 
+  // the inner resources. This is what is known as one-way data flow
+
+  // IMP: states do not come from anywhere like props! 
+  // But in turn we have to set an initial state so that the thing do not crash on the first run  
+  
+  getInitialState: function(){
+   return{
+     filterText: '',
+     inStockOnly: false
+   }; 
+  },
+  //Remember that methods should be separated by comas.
   render: function() {
     return (
       <div className='filterable-table'>
-        <SearchBar />
-        <ProductTable products = {this.props.products} />
+        <SearchBar
+         filterText = {this.state.filterText}
+         inStockOnly={this.state.inStockOnly}
+        />
+        <ProductTable
+         products = {this.props.products}
+         filterText = {this.state.filterText}
+         inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
@@ -25,9 +51,11 @@ var SearchBar = React.createClass({
   render: function() {
     return (
       <form>
-        <input type='text' placeholder='Search product'> </input>
+        <input type='text' placeholder='Search product' value = {this.props.filterText}> </input>
         <br />
-        <input type='checkbox'>Only show products in stock</input>
+        <input type='submit'> </input>
+        <br />
+        <input type='checkbox' value={this.props.inStockOnly}>Only show products in stock</input>
       </form>
     );
   }
@@ -36,16 +64,22 @@ var SearchBar = React.createClass({
 var ProductTable = React.createClass({
   //I use {} only for js expressions that go into
   // child elements and components attributes
+  // indexOf returns the index where the pass string is located in the caller
+  // it returns -1 is the string passed is not in the caller
   render: function() {
     var rows = [];
     var last_category = '';
     this.props.products.forEach(function(product){
+      if (product.name.indexOf(this.props.filterText) === -1 || (this.props.inStockOnly && !product.stocked)){
+        return;
+      }
       if (product.category != last_category){
         rows.push(<CategoryRow category={product.category} />)
       }
       rows.push(<ProductRow name = {product.name} price = {product.price} stocked = {product.stocked} /> );
       last_category = product.category;
-    });
+      //What does bind do? Why does the app breaks without it?
+    }.bind(this));
 
     //how come this stuff gets parse out properly in here if it is passed as an array
     //We can not comment inside react components, it actually gets display in the 
